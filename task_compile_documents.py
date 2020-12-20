3ddae6cf1f6c5c4744e710d409c396c621448986
Tobias Raabe <raabe@posteo.de>
2020-12-20 15:10:54 +0100

    Add pytask.

---
+++ task_compile_documents.py
@@ -0,0 +1,13 @@
+import pytask
+from pathlib import Path
+
+
+ROOT = Path(__file__).parent
+BLD = ROOT / "bld"
+
+
+@pytask.mark.latex
+@pytask.mark.depends_on(ROOT / "paper" / "paper.tex")
+@pytask.mark.produces(BLD / "paper.pdf")
+def task_compile_documents():
+    pass
