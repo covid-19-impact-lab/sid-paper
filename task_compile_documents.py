efe2f7aabcab10a6a5015e124884fff5d1caaffa
Tobias Raabe <raabe@posteo.de>
2020-12-20 16:25:41 +0100

    more fixes.

--- task_compile_documents.py
+++ task_compile_documents.py
@@ -7,7 +7,9 @@ BLD = ROOT / "bld"
 
 
 @pytask.mark.latex
-@pytask.mark.depends_on(ROOT / "paper" / "paper.tex")
+@pytask.mark.depends_on(
+    [ROOT / "paper" / "paper.tex"] + list(ROOT.joinpath("paper").rglob("*.tex"))
+)
 @pytask.mark.produces(BLD / "paper.pdf")
 def task_compile_documents():
     pass
