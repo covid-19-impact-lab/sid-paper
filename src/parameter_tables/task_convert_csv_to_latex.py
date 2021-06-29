3542f93fabd9f84823faef2f0c8a12780f76e26f
Klara Roehrl <klara.roehrl@gmail.com>
2021-06-29 09:12:22 +0200

    Nicely format tables.

--- src/parameter_tables/task_convert_csv_to_latex.py
+++ src/parameter_tables/task_convert_csv_to_latex.py
@@ -12,5 +12,16 @@ PARAMETRIZATION = [(p, BLD / "param_tables" / f"{p.stem}.tex") for p in SRC.iter
 @pytask.mark.parametrize("depends_on, produces", PARAMETRIZATION)
 def task_convert_csv_to_tex(depends_on, produces):
     df = pd.read_csv(depends_on)
+    df = df.dropna(axis=0)  # drop empty rows
     with open(produces, "w") as f:
-        f.write(df.to_latex(na_rep=""))
+        with pd.option_context("max_colwidth", 10000):
+            table = df.to_latex(
+                na_rep="",
+                escape=False,
+                longtable=False,
+                index=False,
+                column_format=r"p{4cm}p{2cm}p{2cm}p{2cm}p{9cm}",
+            )
+        table = table.replace(r"\begin{tabular}", r"\begin{tabular*}{\textwidth}")
+        table = table.replace(r"\end{tabular}", r"\end{tabular*}")
+        f.write(table)
