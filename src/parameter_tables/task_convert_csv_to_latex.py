d11045479d5bb5534bc6b71f3703139a1f7ba0a6
Klara Roehrl <klara.roehrl@gmail.com>
2021-07-10 11:44:14 +0200

    Add first version of estimated parameters table.

--- src/parameter_tables/task_convert_csv_to_latex.py
+++ src/parameter_tables/task_convert_csv_to_latex.py
@@ -13,6 +13,14 @@ PARAMETRIZATION = [(p, BLD / "param_tables" / f"{p.stem}.tex") for p in SRC.iter
 def task_convert_csv_to_tex(depends_on, produces):
     df = pd.read_csv(depends_on)
     df = df.dropna(axis=0, how="all")  # drop empty rows
+    if len(df.columns) == 5:
+        col_format = r"p{4cm}p{2cm}p{2cm}p{2cm}p{9cm}"
+        float_format = "%.0f"
+    elif len(df.columns) == 3:
+        col_format = r"p{3cm}p{1.5cm}p{9cm}"
+        float_format = "%.3f"
+    else:
+        raise ValueError(f"Unexpected number of columns: {len(df.columns)}")
     with open(produces, "w") as f:
         with pd.option_context("max_colwidth", 10000):
             table = df.to_latex(
@@ -20,8 +28,8 @@ def task_convert_csv_to_tex(depends_on, produces):
                 escape=False,
                 longtable=False,
                 index=False,
-                column_format=r"p{4cm}p{2cm}p{2cm}p{2cm}p{9cm}",
-                float_format="%.0f",
+                column_format=col_format,
+                float_format=float_format,
             )
         table = table.replace(r"\begin{tabular}", r"\begin{tabular*}{\textwidth}")
         table = table.replace(r"\end{tabular}", r"\end{tabular*}")
